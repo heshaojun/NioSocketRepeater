d6e5b19d7de4a65211a49d7f8eaaec5ee2306f42
heshaojun <keepword_heshaojun@hotmail.com>
2020-11-18 18:34:23 +0800

    提交部分代码

--- MySocketClient/src/main/java/org/heath/runner/DefaultServerMsgHandler.java
+++ MySocketClient/src/main/java/org/heath/runner/DefaultServerMsgHandler.java
@@ -1,5 +1,6 @@
 package org.heath.runner;
 
+import lombok.extern.log4j.Log4j2;
 import org.heath.common.CommonConst;
 import org.heath.service.AbstractAutoManager;
 import org.heath.service.AbstractServerMsgHandler;
@@ -10,6 +11,7 @@ import org.heath.service.IChannelSelector;
  * @date 2020/11/18
  * @description TODO
  */
+@Log4j2
 public class DefaultServerMsgHandler extends AbstractServerMsgHandler {
     public DefaultServerMsgHandler(IChannelSelector channelSelector, AbstractAutoManager msgClientAutoManager) {
         super(channelSelector, msgClientAutoManager);
